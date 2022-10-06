815ba43e971af9a94fa53860e5e5d09a3d1d5c62
Jefferson Quesado <jeff.quesado@gmail.com>
2022-10-06 18:35:21 -0300

    Adding fork-me and other stuff

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ForkMe } from 'fork-me-corner';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <ForkMe repo="https://github.com/jeffque/peg-kitten-react" />
   </React.StrictMode>
 );
 
